ecd8a7d0cf2b610eff3b95a1e37c037bfdcafbdd
Nicholas Lima de Souza Silva <nicholaslima.contato@outlook.com>
2017-12-16 11:05:45 -0300

    Commit do Fillipe

---
+++ model/domain/Produto.php
@@ -0,0 +1,36 @@
+<?php
+  class Produto{
+    private $id;
+    private $nome;
+    private $preco;
+    private $qtd_estoque;
+
+    public function getId(){
+      return $this->id;
+    }
+    public function getNome(){
+      return $this->nome;
+    }
+    public function getPreco(){
+      return $this->preco;
+    }
+    public function getQTDEstoque(){
+      return $this->qtd_estoque;
+    }
+
+    public function setId($new_id){
+      $this->id = $new_id;
+    }
+    public function setNome($new_nome){
+      $this->nome = $new_nome;
+    }
+    public function setPreco($new_preco){
+      $this->preco = $new_preco;
+    }
+    public function setQTDEstoque($new_qtd_estoque){
+      $this->qtd_estoque = $new_qtd_estoque;
+    }
+
+  }
+  
+?>
