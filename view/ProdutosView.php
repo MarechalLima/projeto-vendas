bca5fc2e95eda69d843f2d612f4f2d41fa87785e
Unknown <eduardo.lucio.correia@gmail.com>
2017-12-17 10:14:28 -0300

    Ajeitando view

--- view/ProdutosView.php
+++ view/ProdutosView.php
@@ -1,1 +1,54 @@
-<?php  ?>
+<?php
+  //require '../model/DAO/ProdutoDAO.php';
+//  require '../model/domain/Produto.php';
+  include 'navbar.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Produtos</title>
+  </head>
+  <body>
+
+    <div class="container">
+
+
+      <ul class="collapsible" data-collapsible="accordion">
+        <li>
+          <div class="collapsible-header" style="display:block">
+            Liquidificador Mallory Flash Black - 2 Velocidades 450W
+            <div class="secondary-content">
+              R$ 49,90
+
+            </div>
+          </div>
+          <div class="collapsible-body">
+            <span class="new badge right" data-badge-caption="em estoque">250</span>
+            <table class="striped bordered">
+              <tr>
+                <th>Voltagem</th>
+                <td>220V</td>
+              </tr>
+              <tr>
+                <th>Potência</th>
+                <td>200W</td>
+              </tr>
+            </table>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $('.collapsible').collapsible();
+        console.log("BOSTA");
+      });
+    </script>
+
+  </body>
+</html>
--- view/navbar.php
+++ view/navbar.php
@@ -10,33 +10,24 @@
         include 'materialize.php';
        ?>
 
-      <nav class="light-blue orange" role="navigation">
+      <nav class="cyan darken-4" role="navigation">
         <div class="nav-wrapper container">
-
           <a id="logo-container" href="#" class="brand-logo">
-            Stoque
-            <img src="images/logo-editop-navbar.png" alt="">
+            TopStock
           </a>
           <ul class="right hide-on-med-and-down">
-            <li><a href="#">Produtos</a></li>
-            <li><a href="#">Historico de Vendas</a></li>
-            <li><a href="#">Meu Perfil</a></li>
-            <li><a href="#">Painel Admin</a></li>
+            <li><a href="#">Estoque</a></li>
+            <li><a href="#">Histórico de vendas</a></li>
+            <li><a href="#">Painel Administrador</a></li>
           </ul>
 
           <ul id="nav-mobile" class="side-nav">
-            <li><a href="#">Produtos</a></li>
-            <li><a href="#">Historico de Vendas</a></li>
-            <li><a href="#">Meu Perfil</a></li>
-            <li><a href="#">Painel Admin</a></li>
+            <li><a href="index.php">Página Inicial</a></li>
+            <li><a href="corretor.php">Corretor</a></li>
+            <li><a href="logout.php">Logout</a></li>
           </ul>
           <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-
-          
-
         </div>
-
-
       </nav>
     </header>
   </body>
