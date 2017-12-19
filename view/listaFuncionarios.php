4db8352099b210e593585ad46b0f114e7afb26c7
Nicholas Lima de Souza Silva <nicholaslima.contato@outlook.com>
2017-12-19 11:25:13 -0300

    Alteração na ordem para exibição

--- view/listaFuncionarios.php
+++ view/listaFuncionarios.php
@@ -17,9 +17,9 @@
           <table class="highlight col-s8 offset-s3">
             <thead>
               <th>Id</th>
+              <th>Login</th>
               <th>Nome</th>
               <th>Cargo</th>
-              <th>Login</th>
             </thead>
             <?php
               $f = new FuncionarioDAO();
@@ -34,15 +34,15 @@
             <tr>
               <td><?= $idFunc ?></td>
               <td><?= $nomeFunc ?></td>
-              <td><?=$cargoFunc ?></td>
-              <td><? $loginFunc ?></td>
+              <td><?= $cargoFunc ?></td>
+              <td><?= $loginFunc ?></td>
               <td>
 
-              <button type="submit" class="btn" name="button"><i class="material-icons">edit</i></button>
-                              <form method="POST" action="deleteFuncionario.php">
-                                  <input type="number" name="idfunc" hidden="true" value="<?= $idFunc ?>">
-                                <button type = "submit" class="btn" name="button"><i class="material-icons">delete</i></button>
-                              </form>
+                <button type="submit" class="btn" name="button"><i class="material-icons">edit</i></button>
+                    <form method="POST" action="deleteFuncionario.php">
+                        <input type="number" name="idfunc" hidden="true" value="<?= $idFunc ?>">
+                        <button type = "submit" class="btn" name="button"><i class="material-icons">delete</i></button>
+                    </form>
               </td>
             </tr>
           <?php } ?>
