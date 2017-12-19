80116be5100d79fe4c7f528efaca40736a92f0ef
Nicholas Lima de Souza Silva <nicholaslima.contato@outlook.com>
2017-12-19 10:01:37 -0300

    Deletando todas as variaveis ao sair do sistema

--- view/logout.php
+++ view/logout.php
@@ -9,6 +9,9 @@
       session_start();
       if ($_SESSION["logado"] == TRUE) {
         unset($_SESSION["logado"]);
+        unset($_SESSION['usuario']);
+        unset($_SESSION['admin']);
+        unset($_SESSION['funcionario']);
         echo "<script>alert('Logout realizado!'); window.location = 'index.php'</script>";
         exit();
       }else {
